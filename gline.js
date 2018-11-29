88ea6b79cd7595a4a642c6ded25c6c662503515d
SensenLiu123 <36674622+SensenLiu123@users.noreply.github.com>
2018-11-29 03:35:04 -0600

    Add files via upload

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','cam.ac.uk','mac.com','indiana.edu','berkeley.edu','uct.ac.za','unicon.net','ucdavis.edu','gmail.com','yale.edu'],
+['2005',57,7,12,12,12,14,6,11,10,5],
+['2006',740,329,308,301,248,230,217,191,163,140]
+];
